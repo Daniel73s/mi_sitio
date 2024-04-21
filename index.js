3678129b494d6ebabdd024175c1524f58a6f8b3a
Daniel Luque <yodaaniel123@gmail.com>
2024-04-20 22:05:54 -0400

    hbs agregado

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 require('dotenv').config()
+const hbs = require('hbs');
 const app = express()
 const port = 3000
 
+// middelwares
+app.use(express.static('public'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
 app.get('/', (req, res) => {
-  res.send('mi propio sitio');
+    res.render('inicio',{
+        titulo:"CreatiWeb"
+    });
+});
+
+app.get('/elements', (req, res) => {
+    res.render('elements',{});
+});
+
+app.get('/generic', (req, res) => {
+    res.render('generic',{});
+});
+
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/404.html');
 })
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT || 3000}`);
+    console.log(`Example app listening on port ${process.env.PORT || 3000}`);
 });
