e9f2f5da13a92af71ef3e14ed40bf72e1de7e0b0
Daniel Luque <yodaaniel123@gmail.com>
2024-04-21 21:58:50 -0400

    ejs implementado en lugar de hbs

---
+++ data/muestras.js
@@ -0,0 +1,40 @@
+listaProductos = [
+    {
+        id: '1',
+        nombre: 'producto1',
+        imagen: 'imagen1',
+        descripcion: 'descripcion1',
+        date:new Date().toDateString()
+    },
+    {
+        id: '2',
+        nombre: 'producto2',
+        imagen: 'imagen2',
+        descripcion: 'descripcion2',
+        date:new Date().toDateString()
+    },
+    {
+        id: '3',
+        nombre: 'producto3',
+        imagen: 'imagen3',
+        descripcion: 'descripcion3',
+        date:new Date().toDateString()
+    },
+    {
+        id: '4',
+        nombre: 'producto4',
+        imagen: 'imagen4',
+        descripcion: 'descripcion4',
+        date:new Date().toDateString()
+    },
+    {
+        id: '5',
+        nombre: 'producto5',
+        imagen: 'imagen5',
+        descripcion: 'descripcion5',
+        date:new Date().toDateString()
+    }
+
+]
+
+module.exports={listaProductos}
--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 const express = require('express');
 require('dotenv').config()
-const hbs = require('hbs');
-const app = express()
-const port = 3000
-
+const { listaProductos } = require('./data/muestras');
+const app = express();
+//motor de plantillas
+app.set('view engine','ejs');
+app.set('views',__dirname+'/views');
+// app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 // middelwares
-app.use(express.static('public'));
-app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials');
-
-
 app.get('/', (req, res) => {
-    res.render('inicio',{
-        titulo:"CreatiWeb"
+    const lista = listaProductos;
+    res.render('index', {
+        titulo: "titulo del sitio",
+        subtitulo: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam saepe labore, nisi ea laborum minima.",
+        fecha_actual: new Date().toDateString(),
+        items: lista
     });
 });
 
-app.get('/elements', (req, res) => {
-    res.render('elements',{});
+app.get('/detalle/:id', (req, res) => {
+    // const id = req.params.id;
+    app.use(express.static(__dirname + '/public'));
+    const item = {
+        // id,
+        nombre: 'nombre de item',
+        descripcion: 'descripcion del item',
+        date: new Date().toDateString(),
+        imagen: 'imagen'
+    }
+    res.render('detalle', {
+        item,
+        titulo:'titulo de la pagina'
+    });
 });
 
-app.get('/generic', (req, res) => {
-    res.render('generic',{});
+app.get('/acercade', (req, res) => {
+    // res.render('elements', {});
+    res.render('acercade');
 });
+
 
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html');
+    res.sendFile(__dirname + "/public/404.html");
 })
 
 app.listen(process.env.PORT, () => {
